4746ef66bed25d27e969166a78b495ae4e639f14
Lucas Machado <lukaasmachado143@gmail.com>
2023-04-15 15:20:23 -0300

    Reescrita da Controller de Funcionários

--- ApiRest/Controllers/FuncionarioController.cs
+++ ApiRest/Controllers/FuncionarioController.cs
@@ -1,4 +1,5 @@
-﻿using AR.Domain.Interfaces.Service;
+﻿using AR.Domain.Entidades;
+using AR.Domain.Interfaces.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiRest.Controllers
@@ -18,10 +19,63 @@ namespace ApiRest.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult> Get() 
+        public async Task<IActionResult> Get()
         {
-            return Ok("Teste da API");
+            try
+            {
+                _logger.LogInformation("Getting employees registred in the database.(Controller)");
+                var ListaGerada = await _funcionarioService.GetAll();
+                if (ListaGerada == null) {
+                    _logger.LogInformation("Non-existent employee list.(Controller)");
+                    return NoContent(); }
+                return Ok(ListaGerada);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error when trying to get all employees registred in the database. Error:{e}. (Controller)");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Error when trying to get all employees registred in the database. Error:{e}.");
+            }
         }
-        
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            try
+            {
+                _logger.LogInformation("Getting employee by id registred in the database.(Controller)");
+                var funcionarioEncontrado = await _funcionarioService.GetById(id);
+                if (funcionarioEncontrado == null) {
+                    _logger.LogInformation("Non-existent employee.(Controller)");
+                    return NoContent(); 
+                }
+                return Ok(funcionarioEncontrado);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error when trying to get employee by id registred in the database. Error:{e}. (Controller)");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Error when trying to get employee by id registred in the database. Error:{e}.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(FuncionarioModel newEmployeeData)
+        {
+            try
+            {
+                _logger.LogInformation("Registering a new employee in the database.(Controller)");
+                var funcionarioRegistrado = _funcionarioService.Add(newEmployeeData);
+                if (funcionarioRegistrado == null) {
+                    _logger.LogInformation("Employee not registred.");
+                    return this.StatusCode(StatusCodes.Status406NotAcceptable, "There was a problem registering the new employee");
+                        }
+                return this.StatusCode(StatusCodes.Status201Created, "Employee registred.(Controller).");            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error when trying to regiter in the database. Error:{e}. (Controller)");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Error when trying to regiter in the database. Error:{e}. Erro:{e}");
+            }
+        }
+
+
     }
 }
